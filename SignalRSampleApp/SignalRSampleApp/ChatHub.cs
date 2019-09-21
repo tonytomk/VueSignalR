3e82717918577a3470362ca7b7beaa9a307590f7
Tony Tom K <tony.tomk@thermofisher.com>
2019-09-21 21:30:54 +0530

    first commit

---
+++ SignalRSampleApp/SignalRSampleApp/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSampleApp
+{
+    public class ChatHub : Hub
+    {
+        
+        public Task Send(string data)
+        {
+            return Clients.All.SendAsync("Send", data);
+        }
+    }
+
+}
